1a93cda6c9032db78d503c35deb282b8c7f72369
etesong014 <etesong0114@gmail.com>
2024-08-31 22:02:07 +0900

    배너 만들기 - api utile 사용해 데이터 불러오기 - 리액트 쿼리 사용을 위한 쿼리 클라이언트 세팅하기

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from './components/Banner/Banner'
 
+// 배너 => popular movie의 첫번째 아이템
+// popular movie
+// top rated movie
+// upcoming movie
 const Homepage = () => {
   return (
     <div>
-      Homepage
+      <Banner />
     </div>
   )
 }
